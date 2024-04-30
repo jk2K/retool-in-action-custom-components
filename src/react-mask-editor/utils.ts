1a920fed10e6d45fec8549953819bfa3d174181a
jk2K <4025839+jk2K@users.noreply.github.com>
2024-04-30 16:01:15 +0800

    fix: mask inverted color

--- src/react-mask-editor/utils.ts
+++ src/react-mask-editor/utils.ts
@@ -9,7 +9,7 @@ export const toMask = (canvas: HTMLCanvasElement) => {
     const origData = Uint8ClampedArray.from(imageData.data);
     if (imageData) {
       for (var i = 0; i < imageData?.data.length; i += 4) {
-        const pixelColor = (imageData.data[i] === 255) ? [255, 255, 255] : [0, 0, 0];
+        const pixelColor = (imageData.data[i] === 255) ? [0, 0, 0] : [255, 255, 255];
         imageData.data[i] = pixelColor[0];
         imageData.data[i + 1] = pixelColor[1];
         imageData.data[i + 2] = pixelColor[2];
